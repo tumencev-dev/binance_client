6fc7fd46061e2872924904dab76853730faa6d78
tumen-dev <dev.tumen@gmail.com>
2021-10-18 02:57:20 +0500

    Функция для воспроизведения аудио

--- test.py
+++ test.py
@@ -4,16 +4,44 @@
 # print(sentence_has_animal('У Ивана есть своя собственная ферма с animals'))
 # print(u'У Ивана есть своя собственная ферма с animals')
 
-import math
+# import math
 
+# flat = [
+#     5.55, 22.19, 7.78, 26.86, 5.55,
+#     29.84, 22.19, 5.55, 16.85, 4.52
+# ]
 
-flat = [
-    5.55, 22.19, 7.78, 26.86, 5.55,
-    29.84, 22.19, 5.55, 16.85, 4.52
-]
-
-sum_area = math.fsum(flat)
-
-print(sum_area)
-
+# sum_area = math.fsum(flat)
+# print(sum_area)
 # https://0.30000000000000004.com/
+
+import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import BUTTON_TYPE_BROWSE_FILE, Open
+import pyglet
+
+def play_audio(file):
+    if file != '':
+        player = pyglet.media.Player()
+        sound = pyglet.media.load(file)
+        player.queue(sound)
+        player.play()
+
+        @player.event
+        def on_eos():
+            pyglet.app.exit()
+
+        pyglet.app.run()
+
+layout = [  [sg.Text('Путь к файлу:')],
+            [sg.InputText(key='-IN-'), sg.FilesBrowse('Открыть')],
+            [sg.Button('Play'), sg.Button('Cancel')] ]
+
+window = sg.Window('Window Title', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    if event == 'Play':
+        play_audio(str(values['-IN-']))
+window.close()
